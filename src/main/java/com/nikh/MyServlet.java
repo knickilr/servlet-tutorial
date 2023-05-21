2da35a752fd8aabf3c6afac4388183c631f775d4
Nikhil <knickilr@gmail.com>
2023-05-21 14:21:09 +0530

    using anotations instead of web.xml

--- src/main/java/com/nikh/MyServlet.java
+++ src/main/java/com/nikh/MyServlet.java
@@ -5,10 +5,12 @@ import java.io.PrintWriter;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/home")
 public class MyServlet extends HttpServlet{
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
 		PrintWriter out = res.getWriter();
--- src/main/java/com/nikh/SqrtServlet.java
+++ src/main/java/com/nikh/SqrtServlet.java
@@ -3,12 +3,14 @@ package com.nikh;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+@WebServlet("/sqr")
 public class SqrtServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse res ) throws IOException {
 		PrintWriter out = res.getWriter();
